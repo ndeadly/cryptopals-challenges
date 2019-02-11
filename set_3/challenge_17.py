e97a640418031b8453c97aab3b787b2290e66057
Nick Todd <ntodd@eyeseetec.de>
2019-02-12 00:23:48 +0100

    Completed set 2, challenge 17

--- set_3/challenge_17.py
+++ set_3/challenge_17.py
@@ -1,7 +1,6 @@
 import base64
 import secrets
 
-
 from set_2.challenge_9 import pkcs7_pad_plaintext
 from set_2.challenge_10 import aes_cbc
 from set_2.challenge_15 import strip_pkcs7_padding, PaddingException
@@ -49,21 +48,50 @@ def main():
     token = generate_session_token()
     iv, ciphertext = token
 
+    plaintext = b''
+
     c1 = iv
     for i in range(0, len(ciphertext), block_size):
+        # Take next block to decrypt
         c2 = ciphertext[i:i+block_size]
-        #print(c1.hex(), c2.hex())
-        c1_mod = bytearray(c1)
-        for j in range(block_size-1, 0, -1):
+
+        # Init plaintext block
+        p2 = bytearray(block_size)
+
+        # Check for last block
+        if i == len(ciphertext) - block_size:
+            for b in range(block_size):
+                c1_mod = bytearray(c1)
+                c1_mod[b] ^= 1
+                if not consume_session_token((iv, c1_mod + c2)):
+                    padding_byte = block_size-b
+                    start_byte = block_size-padding_byte-1
+                    p2[-padding_byte:] = bytes([padding_byte]*padding_byte)
+                    break
+        else:
+            start_byte = block_size-1
+
+        for j in range(start_byte, -1, -1):
+            c1_mod = bytearray(c1)
+
+            padding_byte = block_size-j
+            for b in range(1, padding_byte):
+                c1_mod[-b] ^= p2[-b] ^ padding_byte
+
             for k in range(256):
-                c1_mod[j] = k
-                #print(c1_mod.hex())
+                c1_mod[j] = c1[j] ^ k
                 if consume_session_token((iv, c1_mod + c2)):
-                    dk_c2 = chr(c1_mod[j] ^ (block_size-j))
-                    print(dk_c2)
+                    p2[j] = k ^ padding_byte
                     break
 
+        # Append decrypted block to plaintext
+        plaintext += p2
+        print('Decrypted block:', p2)
+
         c1 = c2
+
+    print()
+    print('Plaintext:', strip_pkcs7_padding(plaintext, block_size).decode())
 
 
 if __name__ == '__main__':
